bff42a49177102cf5eaf09b7045078f85d3e5d30
pupov <pupov435@gmail.com>
2024-06-03 22:10:12 +0700

    initial commit

---
+++ src/main/java/com/ciklon/friendtracker/FriendTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ciklon.friendtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ciklon/friendtracker/FriendTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciklon.friendtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
