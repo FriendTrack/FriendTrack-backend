6730294aa5462f719b65c92945d7576bb69105e1
pupov <pupov435@gmail.com>
2024-06-04 17:05:20 +0700

    Добавил константы для Spring Security

---
+++ src/main/java/com/ciklon/friendtracker/common/security/constant/ApiPaths.java
@@ -0,0 +1,8 @@
+package com.ciklon.friendtracker.common.security.constant;
+
+
+
+public class ApiPaths {
+    public static final String LOGIN = "/api/user/login";
+    public static final String REGISTER = "/api/user/register";
+}
